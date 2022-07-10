383dc7c0dd88acc1c37792e5f87f3bc98ea0fbd1
abinash0013 <74831561+abinash0013@users.noreply.github.com>
2022-07-10 22:41:51 +0530

    Update Featurebox.js

--- src/Components/Featurebox.js
+++ src/Components/Featurebox.js
@@ -4,7 +4,7 @@ function Featurebox(props) {
     return (
         <div className="a-box">
             <div className="a-b-img">
-                <img src={props.image} />
+                <img src={props.image} alt="featureBoximga" />
             </div>
             <div className="a-b-text">
                 <h2>{props.title}</h2>
